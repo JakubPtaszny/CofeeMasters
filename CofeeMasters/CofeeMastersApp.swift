c6c7174da77864f7ee150cd70620c1f7e4660349
Kuba <jakub.ptaszny02@gmail.com>
2023-11-28 00:22:43 +0100

    Initial Commit

---
+++ CofeeMasters/CofeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CofeeMastersApp.swift
+//  CofeeMasters
+//
+//  Created by Jakub Ptaszny on 28/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct CofeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
